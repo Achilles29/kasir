d3d89f18d1e045469aff337029a41f8a48ce7b35
Fuad Muhammad <fuadmuhammadanwar@gmail.com>
2025-06-01 20:33:40 +0700

    update 250601 print kasir

--- application/controllers/Sync_data.php
+++ application/controllers/Sync_data.php
@@ -275,7 +275,7 @@ class Sync_data extends CI_Controller {
             'pr_detail_transaksi_paket',
             'pr_kasir_shift',
             'pr_kasir_shift_log',
-            'pr_voucher'
+            'pr_voucher',
             'pr_log_voucher',
             'pr_pembayaran',
             'pr_refund',
@@ -287,7 +287,7 @@ class Sync_data extends CI_Controller {
             'pr_redeem_log',
             'pr_redeem_setting',
             'pr_stamp_log',
-            'pr_void',
+            'pr_void'
             
         ];
 
